eb9110b94016d5976aa852e8a924221159b02705
rvenkat <rvenkat@MAC-LAP-185.macrosoftindia.com>
2021-04-28 21:25:30 +0530

    First commit

---
+++ src/co/rajaram/venkatraman/IfExample.java
@@ -0,0 +1,13 @@
+package co.rajaram.venkatraman;
+
+//Java Program to demonstrate the use of if statement.  
+public class IfExample {  
+public static void main(String[] args) {  
+  //defining an 'age' variable  
+  int age=20;  
+  //checking the age  
+  if(age>18){  
+      System.out.print("Age is greater than 18");  
+  }  
+}  
+}  
---
+++ src/co/rajaram/venkatraman/WhileExample.java
@@ -0,0 +1,11 @@
+package co.rajaram.venkatraman;
+
+public class WhileExample {  
+	public static void main(String[] args) {  
+	    int i=1;  
+	    while(i<=10){  
+	        System.out.println(i);  
+	    i++;  
+	    }  
+	}  
+	}  
